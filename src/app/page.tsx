3f082de0ff699b426154d2a78b7d24e5465bcb11
Gonzalo corvalan <salvatore.hakase@gmail.com>
2023-05-26 14:29:05 -0300

    optimized shirou

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Image from 'next/image'
+import Modal from '../components/Modal'
+import unlimited from '../../public/unlimited.jpg'
 
 export default function Home() {
   return (
     <main
       className="flex min-h-screen flex-col items-center justify-between p-24"
-      style={{
-        backgroundImage: 'url("/unlimited.jpg")',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center'
-      }}
-    >
 
+    >
+      <Image priority src={unlimited} alt="Shirou from Fate Stay Night" fill style={{ objectPosition: 'center', objectFit: 'cover' }} />
+      <Modal />
     </main>
   )
 }
